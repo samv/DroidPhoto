ff271513702b5287249f64a274288a71ead79ee4
Sam Vilain <sam@vilain.net>
2014-06-14 12:54:43 -0700

    Move layout from main.xml to semantic name

--- src/photo/droid/SearchResultsActivity.java
+++ src/photo/droid/SearchResultsActivity.java
@@ -10,6 +10,6 @@ public class SearchResultsActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_search_results);
     }
 }
