c0b5564646b68e467557d46c2f8f63b99a9d9bf1
Sam Vilain <sam@vilain.net>
2014-06-14 15:30:19 -0700

    Add Json constructor.
    20:51

--- src/photo/droid/ImageResult.java
+++ src/photo/droid/ImageResult.java
@@ -5,6 +5,17 @@ public class ImageResult {
     private String fullUrl;
     private String thumbUrl;
 
+    public ImageResult(JSONObject json) {
+        try {
+            this.fullUrl = json.getString("url");
+            this.thumbUrl = json.getString("tbUrl");
+        }
+        catch (JSONException e) {
+            this.fullUrl = null;
+            this.thumbUrl = null;
+        }
+    }
+
     public String getFullUrl() {
         return fullUrl;
     }
