b1c82268b5dc6916ca225adce14115bd5271187d
Sam Vilain <sam@vilain.net>
2014-06-14 16:22:38 -0700

    Switch to use Jackson JSON library
    Had problems getting this one building from source; use download URLs.

--- ext/mkjar.sh
+++ ext/mkjar.sh
@@ -6,4 +6,9 @@ cd "$(dirname "$0")"
 (cd commons-io; ant jar)
 (cd commons-lang; ant jar)
 (cd scribe-java; mvn source:jar)
+(cd ../lib
+ wget http://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.4.0/jackson-databind-2.4.0.jar
+ wget http://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-core/2.4.0/jackson-core-2.4.0.jar
+ wget http://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.4.0/jackson-annotations-2.4.0.jar
+ )
 
