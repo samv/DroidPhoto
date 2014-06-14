7a7979bf89e3582288d19a731ddc04d7e45eb2ae
Sam Vilain <sam@vilain.net>
2014-06-14 15:08:14 -0700

    Add fields to view class
    13:47

--- src/photo/droid/SearchResultsActivity.java
+++ src/photo/droid/SearchResultsActivity.java
@@ -2,9 +2,16 @@ package photo.droid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
 
 public class SearchResultsActivity extends Activity
 {
+    EditText etQuery;
+    GridView gvResults;
+    Button btnSearch;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
